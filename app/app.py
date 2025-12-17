6a7a0d18ac9edd6404e4391c3b0c3d40a636f2b0
caimath <mathias.carsault@std.heh.be>
2025-12-17 08:30:33 +0100

    test synchro

--- app/app.py
+++ app/app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello():
     return f"Hello from {socket.gethostname()}"
     
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

18bc7c570f7e038a460f548f056447487139bf16
caimath <mathias.carsault@std.heh.be>
2025-12-17 10:40:00 +0100

    fix: problem in app.py

--- app/app.py
+++ app/app.py
@@ -6,10 +6,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return f"Hello from {socket.gethostname()}"
-    
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
